2d269c38637b9a785dd1425f4b59672582f1caf2
shivacharan999 <charan02ch@gmail.com>
2023-12-18 06:09:13 -0800

    M

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,17 +1,26 @@
 data "aws_ami" "ami" {
-    most_recent = true
-    name_regex  = "Centos-8-Devops-Practice"
-    owners   =  ["290972336566"]
+    most_recent  = true
+    name_regex   = "Centos-8-Devops-Practice"
+    owners       =  ["290972336566"]
 }
 
 
 resource "aws_instance" "ec2" {
-    ami      = data.aws_ami.ami.image_id
-    instance_type  = var.instance_type
+    ami                    = data.aws_ami.ami.image_id
+    instance_type          = var.instance_type
     vpc_security_group_ids = [var.sg_id]
     tags = {
         Name = var.component
     }
+}
+
+resource "aws_route35_record" "record" {
+
+    zone_id     = "Z050249122YGPFCQEASIN"
+    name        = "${var.component}-dev.devsig90.online"
+    type        = "A"
+    ttl         = 30
+    record      = [aws_instance.ec2.private_ip]
 }
 
 
