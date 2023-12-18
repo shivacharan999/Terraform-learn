1d43d825c536ebf241c1dbdfbb06d7114b0a03a7
shivacharan999 <charan02ch@gmail.com>
2023-12-18 05:48:00 -0800

    M

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "ec2" {
 
     for_each  = var.instances
     source    = "./ec2"
-    component  = each.value["name]
-    instance_type  = each.value["type]
+    component  = each.value["name"]
+    instance_type  = each.value["type"]
     sg      = module.sg.sg_id
 
 }
